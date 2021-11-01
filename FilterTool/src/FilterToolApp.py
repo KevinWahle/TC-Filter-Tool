6db906ed34ffbaefdaba866dc33fadb37b92161b
Sergio <sergioandyp@gmail.com>
2021-10-31 22:05:06 -0300

    Comienzo de creado de filtros No imprime bien la transferencia
    Co-authored-by: Francisco Basili <FranBasili@users.noreply.github.com>

--- FilterTool/src/FilterToolApp.py
+++ FilterTool/src/FilterToolApp.py
@@ -1,19 +1,20 @@
-from PyQt5 import QtCore
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QListWidgetItem, QMainWindow
+from PyQt5.QtWidgets import QMainWindow
+from src.classes.Filter import Filter
 
 from src.ui.widgets.Main_Window import FilterTool_MainWindow
 
 
 class FilterToolApp(QMainWindow, FilterTool_MainWindow):
     def __init__(self, *args, **kwargs):
+
         # Inicializacion
         super().__init__(*args, **kwargs)
         self.setupUi(self)
 
         # Seteo de visivilidad de widgets
 
-        self.autoSetParamVisiility()
+        self.onFilterChanged(self.Filtro_B.currentIndex())
 
         # Conexiones
 
@@ -22,11 +23,6 @@ class FilterToolApp(QMainWindow, FilterTool_MainWindow):
         self.Edit_Btn.clicked.connect(self.editFilter)
         self.Filtro_B.currentIndexChanged.connect(self.onFilterChanged)
 
-            # Cambio de imagenes
-        filterImg = [ "res/lowpasstemplate.png", "res/highpasstemplate.png",
-                    "res/bandpasstemplate.png", "res/bandstoptemplate.png", "res/groupdelaytemplate.png" ]
-        self.Filtro_B.currentIndexChanged.connect(lambda: self.Filter_Img.setPixmap(QPixmap(filterImg[self.Filtro_B.currentIndex()])))
-
 
         # variables y arreglos
 
@@ -34,8 +30,16 @@ class FilterToolApp(QMainWindow, FilterTool_MainWindow):
 
 
     def onFilterChanged(self, index):
+
+        # Visibilidad de textboxes
+        
         self.Normal_L.setVisible(index in [0, 1])
         self.Pass_Band_L.setVisible(index in [2, 3])
+
+        # Cambio de imagenes
+        filterImg = [ "res/lowpasstemplate.png", "res/highpasstemplate.png",
+                    "res/bandpasstemplate.png", "res/bandstoptemplate.png", "res/groupdelaytemplate.png" ]
+        self.Filter_Img.setPixmap(QPixmap(filterImg[self.Filtro_B.currentIndex()]))
 
     def addFilter(self):
         
@@ -43,46 +47,60 @@ class FilterToolApp(QMainWindow, FilterTool_MainWindow):
 
         name = self.Nombre_T.text()
 
-        filterType = self.Filtro_B.currentIndex()    # Se maneja por indice
-        aprox = self.Aproximacion_B.currentIndex()         # Se maneja por indice
+        filterName = [ "lowpass", "highpass", "bandpass", "bandstop", "groupdelay" ]
+        filterIndex = self.Filtro_B.currentIndex()    # Se maneja por indice
+        filterType = filterName[filterIndex]
+
+        approxName = [ "butter", "bessel", "cheby1", "cheby2", "ellip", "legendre", "gauss"  ]
+        aproxIndex = self.Aproximacion_B.currentIndex()         # Se maneja por indice
+        approx = approxName[aproxIndex]
 
         gain = self.Ganancia_T.value()
-        aten_a = self.Aa_T.value()
-        aten_p = self.Ap_T.value()
-        fpp = self.Fpp_T.value()
-        fpm = self.Fpm_T.value()
-        fap = self.Fap_T.value()
-        fam = self.Fam_T.value()
-        nmin = self.Nmin_T.value()
-        nmax = self.Nmax_T.value()
+        aten = [ self.Ap_T.value(), self.Aa_T.value() ]     # [Ap, Aa]
+        n = [int(self.Nmin_T.value()), int(self.Nmax_T.value())]      # [Nmin, Nmax]
+        
+        
+        if filterType in ["lowpass", "highpass"]:
+            freqs = [ self.Fp_T_2.value(), self.Fa_T_2.value() ]    # [Fp, Fa]
+        else:
+            freqs = [ [ self.Fpm_T.value(), self.Fpp_T.value() ], [self.Fam_T.value(), self.Fap_T.value()] ]    # [ [Fp-, Fp+], [Fa-, Fa+] ]
+        
         qmax = self.Qmax_T.value()
         deonrm = self.Slider.value()
 
-        filterName = [ "lowpass", "highpass", "bandpass", "bandstop", "groupdelay" ]
+
 
         # TODO: LLamar a función que cree el filtro
 
         # DEBUG
         print("Nombre: " + name)
         print("Tipo: " + str(filterType))
-        print("Aproximación: " + str(aprox))
+        print("Aproximación: " + str(approx))
         print("Ganancia: " + str(gain))
-        print("Atenuación A: " + str(aten_a))
-        print("Atenuación P: " + str(aten_p))
-        print("Fpp: " + str(fpp))
-        print("Fpm: " + str(fpm))
-        print("Fap: " + str(fap))
-        print("Fam: " + str(fam))
-        print("Nmin: " + str(nmin))
-        print("Nmax: " + str(nmax))
+        print("Atenuación P: " + str(aten[1]))
+        print("Atenuación A: " + str(aten[0]))
+        # print("Fpm: " + str(freqs[0][0]))
+        # print("Fpp: " + str(freqs[0][1]))
+        # print("Fam: " + str(freqs[1][0]))
+        # print("Fap: " + str(freqs[1][1]))
+        print("Fp: " + str(freqs[0]))
+        print("Fa: " + str(freqs[1]))
+        print("Nmin: " + str(n[0]))
+        print("Nmax: " + str(n[1]))
         print("Qmax: " + str(qmax))
         print("Deonrm: " + str(deonrm))
 
         # Agregar filtro a la lista
+        self.filter.append(Filter(name, filterType, approx, gain, aten, freqs, n, qmax, deonrm/100))
+        self.updateFilterList()
+
+    def updateFilterList(self):
+        self.Filter_List.clear()
+        self.Filter_List.addItems([f.name for f in self.filter])
 
 
     def removeFilter(self):
-        pass
+        self.filter.pop(self.Filter_List.currentRow())
 
     def editFilter(self):
         pass
--- FilterTool/src/classes/Filter.py
+++ FilterTool/src/classes/Filter.py
@@ -1,23 +1,69 @@
+import scipy.signal as ss
+import numpy as np
+
 class Filter:
-    def __init__(self, name, filter_type, approx, gain, Aa, Ap, Fa, Fp, nmin, nmax, qmax):
+    def __init__(self, name, filter_type, approx, gain, aten, freqs, N=None, qmax=None, desnorm=0.5):
         self.name = name
         self.filter_type = filter_type
-        self.approx = approx
+        self.approx = approx        # "butter", "bessel", "cheby1", "cheby2", "ellip", "legendre", "gauss"
         self.gain = gain
-        self.Aa = Aa
-        self.Ap = Ap
-        self.Fa = Fa
-        self.Fp = Fp
-        self.nmin = nmin
-        self.nmax = nmax
+        self.A = aten       # [Ap , Aa]
+        self.freqs = freqs  # [[fp-, fp+], [fa-, fa+] ] o [fp, fa]
+        self.N = N          # [Nmin, Nmax]
         self.qmax = qmax
-        self.nmin = nmin
-        self.nmax = nmax
-        self.qmax = qmax
+        self.desnorm = desnorm
 
-        if (approx == 0):
+        if (approx == 'butter'):
+            ord, wn = ss.buttord(2*np.pi*self.freqs[0], 2*np.pi*self.freqs[1], self.A[0], self.A[1], analog=True)
+            
+            if ord in range(self.N[0], self.N[1]):
+                print("the house is in order")
+                self.sos = ss.butter(ord, wn, btype=self.filter_type, analog=True, output='sos')
+                a, b = ss.butter(ord, wn, btype=self.filter_type, analog=True)
+            else:
+                pass 
+
+        elif (approx == 'cheby1'):
+            ord, wn = ss.cheb1ord(2*np.pi*self.freqs[0], 2*np.pi*self.freqs[1], self.A[0], self.A[1], analog=True)
+            
+            if ord in range(self.N[0], self.N[1]):
+                self.sos = ss.cheby1(ord, self.A[0], wn, btype=self.filter_type, analog=True, output='sos')
+            else:
+                pass
+
+        elif (approx == 'cheby2'):
+            ord, wn = ss.cheb2ord(2*np.pi*self.freqs[1], 2*np.pi*self.freqs[1], self.A[0], self.A[1], analog=True)
+            
+            if ord in range(self.N[0], self.N[1]):
+                self.sos = ss.cheby2(ord, self.A[1], wn, btype=self.filter_type, analog=True, output='sos')
+            else:
+                pass
+
+        elif (approx == 'ellip'):
+            ord, wn = ss.ellipord(2*np.pi*self.freqs[1], 2*np.pi*self.freqs[1], self.A[0], self.A[1], analog=True)
+            
+            if ord in range(self.N[0], self.N[1]):
+                print("the house is in order")
+                self.sos = ss.ellip(ord, self.A[0], self.A[1], wn, btype=self.filter_type, analog=True, output='sos')
+            else:
+                pass
+        # elif (approx == 'bessel'):
+        #     self.sos = ss.bessel(ord, wn, btype=self.filter_type, analog=True, output='sos')
+
+        #     if ord in range(self.N[0], self.N[1]):
+        #         self.sos = ss.bessel(ord, wn, btype=self.filter_type, analog=True, output='sos')
+
+        #     else:
+        #         pass
+            
+        elif (approx == 'legendre'):
             pass
-        elif (approx == 1):
+        elif (approx == 'gauss'):
             pass
-        elif (approx == 2):
-            pass
+        else:
+            raise ValueError("Error en el ingreso de la aproximación")
+        
+        print(ord, wn)
+        print(ss.sos2tf(self.sos))
+        print(self.sos)
+        print(a,b)
